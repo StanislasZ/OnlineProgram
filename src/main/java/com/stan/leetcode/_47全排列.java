6a75c9a0b55872ca7714cf4218add9c9e3444f72
StanislasZ <zrylovestan@163.com>
2019-07-21 19:19:49 +0800

    '0721'

--- src/main/java/com/stan/leetcode/_47全排列.java
+++ src/main/java/com/stan/leetcode/_47全排列.java
@@ -6,51 +6,39 @@ import java.util.List;
 import java.util.Set;
 
 public class _47全排列 {
-    public static void main(String[] args) {
-
-
-    }
-}
-class Solution_47 {
     List<List<Integer>> rlt = new ArrayList<>();
     List<Integer> temp = new ArrayList<>();
+    boolean[] vis;
 
-
-    public List<List<Integer>> permuteUnique(int[] nums) {
-
-
-        boolean[] vis = new boolean[nums.length];
-
-        dfs(nums, vis);
+    public List<List<Integer>> permute(int[] nums) {
+        vis = new boolean[nums.length];
+        dfs(nums, 0);
         return rlt;
-
     }
-    //一搜到底
-    public void dfs(int[] nums, boolean[] vis) {
-        //递归终点
-        if (temp.size() == nums.length) {
+
+    public void dfs(int[] nums, int cnt) {
+
+        if (cnt == nums.length) {
+            //递归终点
             rlt.add(new ArrayList<Integer>(temp));
             return;
         }
 
-        Set<Integer> set = new HashSet<>();   //为了去重
-        for (int i = 0; i < nums.length; i++) {
-
-            if (!vis[i] && !set.contains(nums[i])) {   //这一位数字要是前面已经试过了，就不能再用
-                //若没被使用过
-                temp.add(nums[i]);
+        for (int i = 0; i < nums.length; ++i) {
+            if (!vis[i]) {
                 vis[i] = true;
-                set.add(nums[i]);
-                //进入下层
-                dfs(nums, vis);
+                temp.add(nums[i]);
+
+                dfs(nums, ++cnt);
+
                 //回溯
-                temp.remove(temp.size() - 1);
                 vis[i] = false;
-
+                temp.remove(temp.size() - 1);
+                cnt--;
             }
-
         }
 
 
     }
+
 }
