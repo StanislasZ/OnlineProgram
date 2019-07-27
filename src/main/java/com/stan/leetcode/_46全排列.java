c76087dd8f682d4044232e8ea58cad76c0c733c6
StanislasZ <zrylovestan@163.com>
2019-07-27 20:52:54 +0800

    '0727'

--- src/main/java/com/stan/leetcode/_46全排列.java
+++ src/main/java/com/stan/leetcode/_46全排列.java
@@ -1,4 +1,83 @@
 package com.stan.leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class _46全排列 {
+
+    public static void main(String[] args) {
+        int[] a = new int[]{1,2,3,4};
+        new _46全排列().permute(a);
+    }
+
+    List<List<Integer>> rlt = new ArrayList<>();
+    List<Integer> temp = new ArrayList<>();
+    boolean[] vis;
+
+
+    public List<List<Integer>> permute2(int[] nums) {
+
+        if (nums.length == 0) return rlt;
+        vis = new boolean[nums.length];
+
+        dfs(nums, 0);
+        return rlt;
+    }
+
+    private void dfs(int[] nums, int cnt) {
+        //递归终点
+        if (cnt == nums.length) {
+            rlt.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!vis[i]) {
+                temp.add(nums[i]);
+                vis[i] = true;
+                dfs(nums, ++cnt);
+                //回溯
+                cnt--;
+                temp.remove(temp.size() - 1);
+                vis[i] = false;
+            }
+        }
+    }
+
+    //*****************************************
+    //交换法
+    public List<List<Integer>> permute(int[] nums) {
+
+        if (nums.length == 0) return rlt;
+        permutation(nums, 0);
+        return rlt;
+    }
+
+    /**
+     * 交换法 1 2 3 4
+     * 比如第0位可以是 1 2 3 4的一个，通过交换使第0位变化
+     * 递归时，j从i开始，保证了0到i-1的数不动，保证了不同。
+     * @param nums
+     * @param i
+     */
+    private void permutation(int[] nums, int i) {
+        //递归终点
+        if (i == nums.length) {
+            temp.clear();
+            for (int ele : nums) temp.add(ele);
+            System.out.println(temp);
+            rlt.add(new ArrayList<>(temp));
+        }
+        for (int j = i; j < nums.length; j++) {
+            swap(nums, i, j);
+            //因为j从i开始，所以递归进去后，之后改变后面的，前面（索引0到i-1）不动。
+            permutation(nums, i + 1);
+            swap(nums, i, j);
+        }
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
--- src/main/java/com/stan/leetcode/_47全排列2.java
+++ src/main/java/com/stan/leetcode/_47全排列2.java
@@ -2,47 +2,51 @@ package com.stan.leetcode;
 
 import java.util.*;
 
-public class _47全排列 {
+public class _47全排列2 {
     List<List<Integer>> rlt = new ArrayList<>();
     List<Integer> temp = new ArrayList<>();
-    boolean[] vis;
 
-    public List<List<Integer>> permute(int[] nums) {
-        vis = new boolean[nums.length];
-        dfs(nums, 0);
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        //先排序
+        Arrays.sort(nums);
+
+
+        boolean[] vis = new boolean[nums.length];
+
+        dfs(nums, vis);
         return rlt;
+
     }
-
-    public void dfs(int[] nums, int cnt) {
-
-        if (cnt == nums.length) {
-            //递归终点
+    //一搜到底
+    public void dfs(int[] nums, boolean[] vis) {
+        //递归终点
+        if (temp.size() == nums.length) {
             rlt.add(new ArrayList<Integer>(temp));
             return;
         }
 
-        for (int i = 0; i < nums.length; ++i) {
-            if (!vis[i]) {
-                vis[i] = true;
+        Set<Integer> set = new HashSet<>();   //为了去重
+        for (int i = 0; i < nums.length; i++) {
+
+
+
+            if (!vis[i] && !set.contains(nums[i])) {
+                //若没被使用过
                 temp.add(nums[i]);
-
-                dfs(nums, ++cnt);
-
+                vis[i] = true;
+                set.add(nums[i]);
+                //进入下层
+                dfs(nums, vis);
                 //回溯
-                vis[i] = false;
                 temp.remove(temp.size() - 1);
-                cnt--;
+                vis[i] = false;
+
             }
-        }
-
-
-    }
-
-    public void permutation(int[] nums, int cnt) {
-        //递归终点
-        if (cnt == nums.length) {
 
         }
+
+
     }
 
 }
