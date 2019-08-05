6876542788b071f4f6556e652094610dbea890b9
StanislasZ <zrylovestan@163.com>
2019-08-05 19:03:54 +0800

    '0805'

--- src/main/java/com/stan/al/bishi/DJI/机器人大战.java
+++ src/main/java/com/stan/al/bishi/DJI/机器人大战.java
@@ -6,7 +6,7 @@ public class 机器人大战 {
 
     private static int[][] pass;
     private static boolean[] vis;
-    private static int rlt = Integer.MAX_VALUE;
+    private static int rlt;
     private static int N;
     private static int P;
     private static int C;
@@ -15,79 +15,82 @@ public class 机器人大战 {
 
         Scanner scanner = new Scanner(System.in);
 
-        N = scanner.nextInt();   //路标的数目
-        P = scanner.nextInt();   //通路的条数
-        C = scanner.nextInt();  //测几次
-        pass = new int[N][N];
+        while (scanner.hasNextLine()) {
+            rlt = Integer.MAX_VALUE;
+            N = scanner.nextInt();   //路标的数目
+            P = scanner.nextInt();   //通路的条数
+            C = scanner.nextInt();  //测几次
+            pass = new int[N][N];
 
-        for (int i = 0; i < P; ++i) {
-            int A = scanner.nextInt();
-            int B = scanner.nextInt();
-            int T = scanner.nextInt();
-            pass[A][B] = T;
-            pass[B][A] = T;
-        }
+            for (int i = 0; i < P; ++i) {
+                int A = scanner.nextInt();
+                int B = scanner.nextInt();
+                int T = scanner.nextInt();
+                pass[A][B] = T;
+                pass[B][A] = T;
+            }
 
-        int[] request = new int[C];
-        for (int i = 0; i < C; ++i) {
-            request[i] = scanner.nextInt();
-        }
+            int[] request = new int[C];
+            for (int i = 0; i < C; ++i) {
+                request[i] = scanner.nextInt();
+            }
 
 
-        //第0个点到第0个点是0， 不通的设为int最小值
-        for (int i = 0; i < N; ++i) {
-            for (int j = 0; j < N; ++j) {
-                if (i != j && pass[i][j] == 0) {
-                    pass[i][j] = -1;
+            //第0个点到第0个点是0， 不通的设为int最小值
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j < N; ++j) {
+                    if (i != j && pass[i][j] == 0) {
+                        pass[i][j] = -1;
+                    }
                 }
             }
+
+            vis = new boolean[N];
+
+            int res = 0;
+            int a = 0;
+
+            while (a < C) {
+                rlt = Integer.MAX_VALUE;
+                int target = request[a++];
+                System.out.println("target = " + target);
+
+                dfs(target, 0, 0, 0);
+
+                res = res + rlt;
+
+            }
+            System.out.println(res);
         }
 
-        vis = new boolean[N];
 
 
-        int res = 0;
-        int a = 0;
-        int min = Integer.MAX_VALUE;
-        while (a < C) {
-            rlt = Integer.MAX_VALUE;
-            int target = request[a++];
-
-            dfs(target, 0, 0, 0);
-
-            res = res + rlt;
-
-
-
-
-
-
-
-        }
-        System.out.println(res);
 
     }
 
+    /**
+     * dfs + 回溯
+     * @param target
+     * @param pre
+     * @param curr
+     * @param cnt
+     */
     private static void dfs(int target, int pre, int curr, int cnt) {
+
+        vis[curr] = true;
+        cnt = cnt + pass[pre][curr];
         //递归终点
         if (curr == target) {
-            vis[curr] = true;
-            if (pass[pre][curr] != -1)
-
-                rlt = Math.min(rlt, cnt + pass[pre][curr]);
+            rlt = Math.min(rlt, cnt);
             return;
         }
 
         for (int i = 0; i < N; ++i) {
-            if (!vis[i]) {
-                vis[i] = true;
-                if (pass[pre][i] == -1) {
-                    return;
-                } else {
-                    dfs(target, curr, i, cnt + pass[pre][curr]);
-                    //回溯
-                    vis[i] = false;
-                }
+            //没走过的 且 可以从curr走到i
+            if (!vis[i] && pass[curr][i] != -1) {
+                dfs(target, curr, i, cnt);
+                //回溯
+                vis[i] = false;
             }
         }
     }
