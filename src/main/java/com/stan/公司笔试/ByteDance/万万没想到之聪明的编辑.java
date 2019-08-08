a0a12aeaff4608810edc9b0811f0b4841a238130
StanislasZ <zrylovestan@163.com>
2019-08-08 12:23:19 +0800

    '0808'

--- src/main/java/com/stan/公司笔试/ByteDance/万万没想到之聪明的编辑.java
+++ src/main/java/com/stan/公司笔试/ByteDance/万万没想到之聪明的编辑.java
@@ -13,6 +13,7 @@ public class 万万没想到之聪明的编辑 {
         Solution_万万没想到之聪明的编辑 s = new Solution_万万没想到之聪明的编辑();
         for (int i = 0; i < arr.length; ++i) {
             arr[i] = scanner.nextLine();
+            //System.out.println("原字符串 = " + arr[i]);
             System.out.println(s.process(arr[i]));
         }
         scanner.close();
@@ -26,39 +27,47 @@ class Solution_万万没想到之聪明的编辑 {
     public String process(String str) {
 
         char[] arr = str.toCharArray();
+        int N = arr.length;
         if (arr.length <= 2) return str;
-        StringBuilder sb = new StringBuilder(str);
+        StringBuilder sb = new StringBuilder();
         int left = 0;
         int right = 1;
-        while (right < arr.length) {
-            if (left >= arr.length - 2) {
+        while (left < N) {
+            //前后2个不同 或 left已经是最后一个索引， 直接加arr[left] 双指针后移
+            if (left == N - 1 || arr[left] != arr[right]) {
                 sb.append(arr[left]);
-                continue;
-            }
-            while (arr[right] != left && right < arr.length - 1) right++;
-
-            if (right - left == 1) {
-                sb.append(arr[left]);
-                left = right;
-                right = left + 1;
+                ++ left;
+                ++ right;
             } else {
-                //2 3 4...都变成2个
-                sb.append(left);
-                sb.append(left);
-                //变成2个后，检查是否存在aabb
-                sb.append(arr[right]);
-                if (right + 1 < arr.length && arr[right] == arr[right + 1]) {
+                //.....aaabbbbc......为例，要变成.....aabc.....
+                sb.append(arr[left]);
+                sb.append(arr[left]);
 
-                    while (arr[right + 1] != arr[right] && right + 1 < arr.length - 1) right++;
+                int n = getNextDifferent(arr, right);  //第一个b的索引
+                int nn = getNextDifferent(arr, n);  //第一个c的索引
 
-                }
-                left = right + 1;
+                if (n < N) sb.append(arr[n]);
+                left = nn;
                 right = left + 1;
+
+
             }
 
         }
         return sb.toString();
 
 
+    }
+
+    private int getNextDifferent(char[] arr, int curr) {
+        if (curr >= arr.length) return arr.length;
+        int next = curr + 1;
+        while (next < arr.length) {
+            if (arr[next] != arr[curr]) {
+                break;
+            }
+            ++ next;
+        }
+        return next;
     }
 }
