7d32efe3f40dc480297cd081e8e8e46bad48a4c7
StanislasZ <zrylovestan@163.com>
2019-08-22 12:19:39 +0800

    '0822'

--- src/main/java/com/stan/leetcode/_127单词接龙.java
+++ src/main/java/com/stan/leetcode/_127单词接龙.java
@@ -1,6 +1,8 @@
 package com.stan.leetcode;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class _127单词接龙 {
 
@@ -47,10 +49,57 @@ public class _127单词接龙 {
 
     }
 
+    /**
+     * 递归
+     * bfs -> 双端bfs -> 临近点查找方式
+     * @param beginWord
+     * @param endWord
+     * @param wordList
+     * @return
+     */
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
 
+        if (wordList == null || wordList.size() == 0) return 0;
+        //起点
+        Set<String> start = new HashSet<>();
+        //终点
+        Set<String> end = new HashSet<>();
+        //方法参数 list -> hashset
+        Set<String> dict = new HashSet<>(wordList);
+        start.add(beginWord);
+        end.add(endWord);
+        if (! dict.contains(endWord)) return 0;
+        return bfs(start, end, dict, 2);
+    }
 
-        return -1;
+    public int bfs(Set<String> start, Set<String> end, Set<String> dict, int cnt) {
+        //双端查找，若任意一段出现断裂，则不存在
+        if (start.size() == 0) return 0;
+        //优化：用少的去找多的
+        if (start.size() > end.size()) return bfs(end, start, dict, cnt);
 
+        //bfs标记行为：只能用一次
+        dict.removeAll(start);
+        //收集下一层临近点
+        Set<String> next = new HashSet<>();
+        for (String s : start) {
+            char[] arr = s.toCharArray();
+            for (int i = 0; i < arr.length; ++i) {
+                char tmp = arr[i];
+                //变化
+                for (char c = 'a'; c <= 'z'; ++c) {
+                    if (tmp == c) continue;
+                    arr[i] = c;
+                    String next_str = new String(arr);
+                    if (dict.contains(next_str)) {
+                        if (end.contains(next_str)) return cnt;
+                        else next.add(next_str);
+                    }
+                }
+                //复原
+                arr[i] = tmp;
+            }
+        }
+        return bfs(next, end, dict, cnt + 1);
     }
 }
