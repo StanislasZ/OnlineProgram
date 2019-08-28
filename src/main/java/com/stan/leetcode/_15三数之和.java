d1ae19542ef7aa7b4c26254a96ee62741649b11f
StanislasZ <zrylovestan@163.com>
2019-08-28 19:33:35 +0800

    '0827'

--- src/main/java/com/stan/leetcode/_15三数之和.java
+++ src/main/java/com/stan/leetcode/_15三数之和.java
@@ -8,25 +8,56 @@ public class _15三数之和 {
 
 
     public static void main(String[] args) {
-        int[] a= new int[]{-1, 0, 1, 2, -1, -4};
-        System.out.println(new _15三数之和().threeSum(a));
+        int[] a = new int[]{-1, 0, 1, 2, -1, -4};
+//        System.out.println(new _15三数之和().threeSum(a));
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) break;  //i<L<R i大于0后面也大于0  不能加起来=0
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int L = i + 1;
+            int R = nums.length - 1;
+            while (L < R) {
+                int sum = nums[i] + nums[L] + nums[R];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[L], nums[R]));
+                    while (L < R && nums[L] == nums[L + 1]) L++;
+                    while (L < R && nums[R] == nums[R - 1]) R--;
+                    R--;
+                    L++;
+                } else if (sum < 0) L++;
+                else                R--;
+            }
+        }
+        return res;
+
     }
 
 
+    //*********************************************************************
 
-    List<List<Integer>> rlt=new ArrayList<>();
+    List<List<Integer>> rlt = new ArrayList<>();
     List<Integer> temp = new ArrayList<>();
 
 
-    public List<List<Integer>> threeSum(int[] nums) {
+    /**
+     * 超时
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum2(int[] nums) {
 
         if (nums.length <= 2) return rlt;
         Arrays.sort(nums);
 
         dfs(nums, 0, 0, 0);
         return rlt;
-
-
     }
 
     private void dfs(int[] nums, int cnt, int curr, int sum) {
