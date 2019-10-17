6e3d155fbdc174e7da149ece371220794500397d
StanislasZ <zrylovestan@163.com>
2019-10-17 10:34:09 +0800

    '1017'

--- src/main/java/com/stan/leetcode/_300最长上升子序列.java
+++ src/main/java/com/stan/leetcode/_300最长上升子序列.java
@@ -1,13 +1,66 @@
 package com.stan.leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class _300最长上升子序列 {
     public static void main(String[] args) {
-        int[] arr = new int[]{2,2};
-        System.out.println(new Solution_300().lengthOfLIS2(arr));
+        int[] arr = new int[]{4,3,1,7,8,9};
+//        System.out.println(new Solution_300().lengthOfLIS2(arr));
+
+        System.out.println(new Solution_300().LISDetail(arr));
+
 
     }
 }
 class Solution_300 {
+
+    /**
+     * 动态规划
+     * for循环中维护4个变量：
+     *      dp_len[i]：0到i位取lis，以i为结尾能取到的最大长度
+     *      pre[i]：本次以i为结果，存上一个过来的位置
+     *      result：遍历到目前的最大长度
+     *      index：遍历到目前的最长lis序列的最后一位索引
+     *
+     * @param nums
+     * @return： 返回最长上升子序列的具体list
+     */
+    public ArrayList<Integer> LISDetail(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return null;
+        int[] dp_len = new int[nums.length];
+        int[] pre = new int[nums.length];
+
+        Arrays.fill(dp_len, 1);  //初始化
+        Arrays.fill(pre, -1);    //初始化
+        int result = 1;
+        int index = 0;
+        for(int i = 1; i < nums.length; ++i) {
+
+            for(int j = 0; j < i; j++) {
+                if (nums[j] < nums[i] && dp_len[j] + 1 > dp_len[i]) {
+                    dp_len[i] = dp_len[j] + 1;
+                    pre[i] = j;
+                }
+            }
+            //更新lis序列的最后一位索引
+            if(result < dp_len[i]) {
+                result = dp_len[i];
+                index = i;
+            }
+        }
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        while (index >= 0) {
+            res.add(nums[index]);
+            index = pre[index];
+        }
+        Collections.reverse(res);
+        return res;
+    }
+
+
 
 
     /**
