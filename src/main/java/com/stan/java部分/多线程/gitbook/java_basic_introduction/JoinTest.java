2ec5dda62a53a2000124b6325d2aa40e2dba70d7
StanislasZ <382698149@qq.com>
2019-11-09 20:33:29 +0800

    '1109'

---
+++ src/main/java/com/stan/java部分/多线程/gitbook/java_basic_introduction/JoinTest.java
@@ -0,0 +1,53 @@
+package com.stan.java部分.多线程.gitbook.java_basic_introduction;
+
+public class JoinTest {
+
+
+    /*
+        当在某个线程中调用其他线程的join()方法时，
+        调用的线程将被阻塞，
+        直到被join()方法加入的线程执行完成后它才会继续运行
+
+        让别人插个队
+
+        场景？
+            线程2需要的数据 必须等线程1结束了 才是正确的
+     */
+
+    public static void main(String[] args) {
+
+        Worker worker1 = new Worker("work-1");
+        Worker worker2 = new Worker("work-2");
+
+        worker1.start();
+        System.out.println("启动线程1");
+        try {
+            worker1.join();
+            //等worker1这个线程run方法执行结束，main才会继续
+            System.out.println("启动线程2");
+            worker2.start();
+            worker2.join();
+            //等worker2这个线程run方法执行结束，main才会继续
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("主线程继续执行");
+    }
+}
+class Worker extends Thread {
+
+    public Worker(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("work in " + getName());
+    }
+}
---
+++ src/main/java/com/stan/leetcode/_227基本计算器.java
@@ -0,0 +1,19 @@
+package com.stan.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class _227基本计算器 {
+
+
+    public static void main(String[] args) {
+        System.out.println(new _227基本计算器().calculate("1"));
+    }
+    public int calculate(String s) {
+
+        //todo
+
+        return -1;
+    }
+}
