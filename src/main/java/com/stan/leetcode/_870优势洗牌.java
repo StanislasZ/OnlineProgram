52139b272807b65c312f0959827d38d56f23e690
StanislasZ <382698149@qq.com>
2019-11-12 21:54:52 +0800

    '1112'

--- src/main/java/com/stan/leetcode/_870优势洗牌.java
+++ src/main/java/com/stan/leetcode/_870优势洗牌.java
@@ -4,39 +4,42 @@ import java.util.*;
 
 public class _870优势洗牌 {
 
+    public static void main(String[] args) {
+        int[] A = new int[]{2,7,11,15};
+        int[] B = new int[]{1,10,4,10};
+
+        int[] res = new _870优势洗牌().advantageCount(A, B);
+        for (int ele : res) {
+            System.out.println(ele);
+        }
+
+    }
+
     public int[] advantageCount(int[] A, int[] B) {
 
         int[] sortedA = A.clone();
         Arrays.sort(sortedA);
-        int[] sortedB = B.clone();
+        int[] sortedB = B.clone();   //最后要按顺序
         Arrays.sort(sortedB);
 
         // assigned[b] = list of a that are assigned to beat b
-        Map<Integer, Deque<Integer>> assigned = new HashMap();
+        Map<Integer, Queue<Integer>> assigned = new HashMap<>();
         for (int b: B) assigned.put(b, new LinkedList());
 
-        // remaining = list of a that are not assigned to any b
-        Deque<Integer> remaining = new LinkedList();
 
-        // populate (assigned, remaining) appropriately
-        // sortedB[j] is always the smallest unassigned element in B
+        Queue<Integer> remaining = new LinkedList();
+
         int j = 0;
         for (int a: sortedA) {
-            if (a > sortedB[j]) {
-                assigned.get(sortedB[j++]).add(a);
-            } else {
-                remaining.add(a);
-            }
+            if (a > sortedB[j]) assigned.get(sortedB[j++]).add(a);
+            else remaining.add(a);
         }
+        System.out.println("assgin = " + assigned);
 
-        // Reconstruct the answer from annotations (assigned, remaining)
         int[] ans = new int[B.length];
         for (int i = 0; i < B.length; ++i) {
-            // if there is some a assigned to b...
-            if (assigned.get(B[i]).size() > 0)
-                ans[i] = assigned.get(B[i]).pop();
-            else
-                ans[i] = remaining.pop();
+            if (assigned.get(B[i]).size() > 0) ans[i] = assigned.get(B[i]).poll();
+            else ans[i] = remaining.poll();
         }
         return ans;
     }
