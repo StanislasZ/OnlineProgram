35b8776bcdd5dab9f36808d5376af45a257308fd
StanislasZ <382698149@qq.com>
2019-11-20 19:42:18 +0800

    '1120'

--- src/main/java/com/stan/leetcode/_76最小覆盖子串.java
+++ src/main/java/com/stan/leetcode/_76最小覆盖子串.java
@@ -1,28 +1,90 @@
 package com.stan.leetcode;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class _76最小覆盖子串 {
 
+
+    /**
+     * 有空再看
+     *
+     *
+     * 比如t是"aa" 那s中的子串必须也包含2个a， 不能1个a
+     * @param s
+     * @param t
+     * @return
+     */
     public String minWindow(String s, String t) {
 
-        //特殊输入
-        if (t.isEmpty() || t.length() > s.length()) return "";
-        String ans = "";
-        int cnt = 0, left = 0, right = 0, minLen = s.length() + 1;
-        Map<Character, Integer> map = new HashMap<>();
+        if (s.length() == 0 || t.length() == 0) return "";
 
-//        for (int i = 0; i < )
-        // todo
+        Map<Character, Integer> dictT = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            int count = dictT.getOrDefault(t.charAt(i), 0);
+            dictT.put(t.charAt(i), count + 1);
+        }
 
-        return "";
+        // Number of unique characters in t, which need to be present in the desired window.
+        int required = dictT.size();
+
+        int l = 0, r = 0;
+
+        // formed is used to keep track of how many unique characters in t
+        // are present in the current window in its desired frequency.
+        // e.g. if t is "AABC" then the window must have two A's, one B and one C.
+        // Thus formed would be = 3 when all these conditions are met.
+        int formed = 0;
+
+        // Dictionary which keeps a count of all the unique characters in the current window.
+        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
+
+        // ans list of the form (window length, left, right)
+        int[] ans = {-1, 0, 0};
+
+        while (r < s.length()) {
+            // Add one character from the right to the window
+            char c = s.charAt(r);
+            int count = windowCounts.getOrDefault(c, 0);
+            windowCounts.put(c, count + 1);
+
+            // If the frequency of the current character added equals to the
+            // desired count in t then increment the formed count by 1.
+            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
+                formed++;
+            }
+
+            // Try and co***act the window till the point where it ceases to be 'desirable'.
+            while (l <= r && formed == required) {
+                c = s.charAt(l);
+                // Save the smallest window until now.
+                if (ans[0] == -1 || r - l + 1 < ans[0]) {
+                    ans[0] = r - l + 1;
+                    ans[1] = l;
+                    ans[2] = r;
+                }
+
+                // The character at the position pointed by the
+                // `Left` pointer is no longer a part of the window.
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
+                    formed--;
+                }
+
+                // Move the left pointer ahead, this would help to look for a new window.
+                l++;
+            }
+
+            r++;
+        }
+
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+
+
     }
 
     public static void main(String[] args) {
-        String s = "ADOBECODEBANC";
-        String t = "ABC";
-       // new _76最小覆盖子串().minWindow(s, t);
+        String s = "aa";
+        String t = "aa";
+        System.out.println(new _76最小覆盖子串().minWindow(s, t));
     }
 }
