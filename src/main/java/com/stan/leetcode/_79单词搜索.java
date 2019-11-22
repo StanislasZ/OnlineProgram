880163d9f8139b536e08142d15ca6ce6b66f442b
StanislasZ <382698149@qq.com>
2019-11-22 10:53:42 +0800

    '1122'

--- src/main/java/com/stan/leetcode/_79单词搜索.java
+++ src/main/java/com/stan/leetcode/_79单词搜索.java
@@ -2,6 +2,15 @@ package com.stan.leetcode;
 
 public class _79单词搜索 {
 
+
+    public static void main(String[] args) {
+
+        char[][] board = new char[][]{{'a'}};
+        String word = "ab";
+        System.out.println(new _79单词搜索().exist(board, word));
+    }
+
+
     int rows;
     int cols;
     boolean[][] vis;
@@ -10,10 +19,11 @@ public class _79单词搜索 {
 
         rows = board.length;
         cols = board[0].length;
+        System.out.println("rows = " + rows + ", cols = " + cols);
         vis = new boolean[rows][cols];
 
         for (int i = 0; i < rows; ++i) {
-            for (int j = 0; i < cols; ++j) {
+            for (int j = 0; j < cols; ++j) {
                 if (dfs(board, i, j, word, 0)) return true;
             }
         }
@@ -23,9 +33,14 @@ public class _79单词搜索 {
     private boolean dfs(char[][] board, int row, int col, String word, int i) {
 
         //递归终点
+        if (i == word.length()) return true;
+
         if (row < 0 || row >= rows || col < 0 || col >= cols) return false;
         if (vis[row][col]) return false;
         if (word.charAt(i) != board[row][col]) return false;
+
+
+
 
         //这一格对了
         vis[row][col] = true;
--- src/main/java/com/stan/公司笔试/第四范式/LRU.java
+++ src/main/java/com/stan/公司笔试/第四范式/LRU.java
@@ -1,8 +1,6 @@
 package com.stan.公司笔试.第四范式;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class LRU {
 
@@ -15,26 +13,47 @@ public class LRU {
 
         in.nextLine();
 
-        //String[] arr = new String[N];
         int i = 0;
+        int score = 1;
+        Map<Integer, Integer> score_map = new HashMap<>();
 
+        Map<Integer, Integer> map = new TreeMap<>(Comparator.comparingInt(x -> score_map.get(x)));
 
-        Map<Integer, Integer> map = new HashMap<>();
 
         while (in.hasNextLine()) {
             String s = in.nextLine();
             String[] arr = s.split(" ");
 
             int key = Integer.parseInt(arr[1]);
-            int val = Integer.parseInt(arr[2]);
+            int val = 0;
 
             if (arr[0].equals("PUT")) {
+                val = Integer.parseInt(arr[2]);
+                if (map.size() == M) {
+                    //满了
+                    //找到最旧的
+                    int k = map.keySet().iterator().next();
+                    System.out.println("现在最老的 key = " + k);
+                    //删去
+                    map.remove(k);
+                    score_map.remove(k);
 
+                }
+                //新的加进来
+                score_map.put(key, ++score);
+                map.put(key, val);
+
+            } else {
+
+                if (map.get(key) != null) {
+                    score_map.put(key, ++ score);
+                    val = map.get(key);
+                    map.remove(key);
+                    map.put(key, val);
+
+                }
+                System.out.println(map.get(key));
             }
-
-
-
-
 
             ++ i;
         }
@@ -43,7 +62,20 @@ public class LRU {
 
     }
 }
-class Node {
-    int val;
-    int cnt;
-}
+//class Node {
+//    int val;
+//    int cnt;
+//
+//    public Node(int val, int cnt) {
+//        this.val = val;
+//        this.cnt = cnt;
+//    }
+//
+//    public int getVal() {
+//        return val;
+//    }
+//
+//    public int getCnt() {
+//        return cnt;
+//    }
+//}
