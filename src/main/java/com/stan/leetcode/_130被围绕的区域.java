9bedd1be8a328b8ab3ccc5ffea5a436a730b1565
StanislasZ <382698149@qq.com>
2019-11-23 15:58:30 +0800

    '1123'

--- src/main/java/com/stan/leetcode/_130被围绕的区域.java
+++ src/main/java/com/stan/leetcode/_130被围绕的区域.java
@@ -5,6 +5,65 @@ import java.util.List;
 
 public class _130被围绕的区域 {
 
+
+    /**
+     * 刚才的思路不太好
+     * 改进思路：
+     * 从最外面的边上的'O'开始dfs，联通的'O'都是不用变'X'的
+     * 全部搞完后，剩下的所有'O'改成'X’即可
+     *
+     * 应该能快点吧
+     *
+     * 果然，击败98%，漂亮！
+     *
+     * @param board
+     */
+    public void solve2(char[][] board) {
+        rows = board.length;
+        if (rows == 0) return;
+        cols = board[0].length;
+        vis = new boolean[rows][cols];
+
+        //4个for对应最外面4条边
+        for (int j = 0; j < cols; ++j) dfs2(board, 0, j);
+        for (int j = 0; j < cols; ++j) dfs2(board, rows - 1, j);
+
+        for (int i = 0; i < rows; ++ i) dfs2(board, i, 0);
+        for (int i = 0; i < rows; ++ i) dfs2(board, i, cols - 1);
+
+        //最后把没访问到的O变成x
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (!vis[i][j]) board[i][j] = 'X';
+            }
+        }
+    }
+
+    private void dfs2(char[][] board, int row, int col) {
+        if (row < 0 || row >= rows || col < 0 || col >= cols) return ;
+        //访问过
+        if (vis[row][col]) return;
+        //x跳过
+        if (board[row][col] == 'X') return;
+
+        vis[row][col] = true;
+
+        dfs2(board, row + 1, col);
+        dfs2(board, row - 1, col);
+        dfs2(board, row, col + 1);
+        dfs2(board, row, col - 1);
+
+        //不用回溯
+    }
+
+
+
+
+
+
+
+    //*************************************************************************
+
     int rows;
     int cols;
 
